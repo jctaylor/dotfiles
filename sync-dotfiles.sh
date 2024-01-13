6b8adee3164658ba4652d862fef030280c716bbd
Jason Taylor <jasontaylor70@gmail.com>
2024-01-13 15:31:39 -0500

    Finished the sync-dotfiles script

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 #
+#
+script_name=$(realpath $0)
+script_dir=$( dirname $script_name )
+script_name=$( basename $script_name )
 
 usage="
-Usage: $(basename $(realpath $0)) [--help]
+Usage: $script_name [--help]
 
 Sync files/subdirectories) to \${HOME} directory.
 
 "
+debug=debug
+function log {
+    if [ "$debug" = debug ]; then
+        echo "$*"
+    fi
+}
+
+cd $script_dir
 
 for src_dir in $(find home -type d); do
     dir_path=$( echo $src_dir | sed "s#.*home/#${HOME}/#" )
-    echo "create dir would be mkdir -p $dir_path"
+    log "Checking that $dir_path  exits"
+    if [ -n "$dir_path" ] && [ ! -d $dir_path ]; then
+        mkdir -p "$dir_path" from "$src_dir"
+    else
+        log "$dir_path already exists"
+    fi
 done
 
+
 for src_file in $(find home -type f); do
-    file=$( sed "s#.*home/#{${HOME}/#" )
-    echo "CMD: ln -f -n --backup=numbered $src_file $file"
+    file=$( echo $src_file | sed "s#.*home/#${HOME}/#" )
+    src_file=$(realpath $src_file)
+    log "CMD: ln -f -n $src_file $file"
+    ln -f -n "$src_file" "$file"
 done
 
