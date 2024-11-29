8e72391741b792c999cbe32b6d482bcae820ccc8
Jason Taylor <jasontaylor70@gmail.com>
2024-11-29 17:34:23 -0500

    Defined :Q as :q and similar commands. Comments in tmux config

--- home/.config/nvim/lua/user/settings.lua
+++ home/.config/nvim/lua/user/settings.lua
@@ -36,7 +36,14 @@ vim.opt.updatetime = 50
 vim.opt.whichwrap = "bs<>[]hl"
 vim.opt.wrap = true
 vim.opt.writebackup = false
+vim.opt.colorcolumn = "120" -- This is a string. In general it is a list of columns seperated by commas
 
+
+vim.api.nvim_create_user_command('WQ', 'wq', {})
+vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Qa', 'qa', {})
+vim.api.nvim_create_user_command('Q', 'q', {})
 
 
 
