3250c462166d7191083dee3a01e2a926a54a5626
Jason Taylor <jasontaylor70@gmail.com>
2026-01-03 15:02:17 -0500

    Enabled which-key

--- home/.config/nvim/lua/user/plugins/which-key.lua
+++ home/.config/nvim/lua/user/plugins/which-key.lua
@@ -1,11 +1,20 @@
 return {
     "folke/which-key.nvim",
-    enabled = false,
+    enabled = true,
     event = "VeryLazy",
     init = function()
         vim.o.timeout = true
-        vim.o.timeoutlen = 500
+        vim.o.timeoutlen = 1000
     end,
     opts = {
     },
+    key = {
+        {
+            "<Space>?",
+            function()
+                require("which-key").show({global = false})
+            end,
+            desc = "Buffer Local Keymaps (which-key)"
+        }
+    }
 }
